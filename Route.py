75d89d787f2eb422e850edc1fc480c75be7bf07c
MattTokunaga <matttokunaga1@gmail.com>
2024-06-20 15:50:35 -0700

    added a bunch of route getter methods

--- Route.py
+++ Route.py
@@ -12,8 +12,11 @@ class Route:
             self.player = Player(self.player_id)
         self.player.add_position(self.player_position)
         self.player.add_level(play["game_str"].iloc[0][-2:])
+        self.player.add_route(self)
         self.df = self.create_df(play, player_pos)
 
+    # not a getter for df instance variable
+    # intended to be used to create df variable at time of initialization
     def create_df(self, play, player_pos):
         contact_timestamp = play[(play["player_position"] == 10) & (play["event_code"] == 4)]["timestamp"].iloc[0]
         retrieval_timestamp = play[(play["player_position"] == self.player_position) & (play["event_code"] == 2)]["timestamp"].iloc[0]
@@ -22,6 +25,7 @@ class Route:
             (player_pos["timestamp"] >= contact_timestamp) & 
             (player_pos["timestamp"] <= retrieval_timestamp)]
         
+    # not a getter for player id, used at initialization to find id
     def find_player_id(self, play, game_info):
         df = play.merge(game_info, on = "play_per_game")
         if self.player_position == 7:
@@ -33,3 +37,70 @@ class Route:
         else:
             print("Error: Position not 7, 8, or 9")
             return False
+        
+    # getter for player id
+    # goes through player object
+    def get_player_id(self):
+        return self.player.get_id()
+    
+    # getter for player's position during this route
+    # does not go through player object
+    def get_current_player_position(self):
+        return self.player_position
+
+    # getter for player's positions
+    # goes through player object
+    def get_player_positions(self):
+        return self.player.get_positions()
+    
+    # getter for dataframe
+    def get_df(self):
+        return self.df
+
+    # getter for coordinate tuples
+    # not a pre-existing instance variable
+    # created from df instance variable
+    def get_coord_tuples(self):
+        df = self.get_df()
+        return list(zip(list(df["field_x"]), list(df["field_y"])))
+    
+    # getter for list of x coordinates
+    # not a pre-existing instance variable
+    # created from df instance variable
+    def get_x_coords(self):
+        return list(self.get_df()["field_x"])
+    
+    # getter for list of x coordinates
+    # not a pre-existing instance variable
+    # created from df instance variable
+    def get_y_coords(self):
+        return list(self.get_df()["field_y"])
+    
+    # getter for starting coordinates
+    # not a pre-existing instance variable
+    def get_start_coords(self):
+        return (self.get_df()["field_x"].iloc[0], self.get_df()["field_y"].iloc[0])
+    
+    # getter for retrieval coordinates
+    # not a pre-existing instance variable
+    def get_retrieval_coords(self):
+        return (self.get_df()["field_x"].iloc[-1], self.get_df()["field_y"].iloc[-1])
+    
+    # getter for total route length
+    # linearly interpolates between points
+    # not a pre-existing instance variable
+    def get_total_length(self):
+        coords = self.get_coord_tuples()
+        total = 0
+        for i in range(len(coords) - 1):
+            total += ((coords[i][0] - coords[i+1][0])**2 + (coords[i][1] - coords[i+1][1])**2)**.5
+        return total
+    
+    # getter for ideal length
+    # straight line distance from start coords to retrieval coords
+    # not a pre-existing instance variable
+    def get_ideal_length(self):
+        start = self.get_start_coords()
+        retrieve = self.get_retrieval_coords()
+        return ((start[0] - retrieve[0])**2 + (start[1] - retrieve[1])**2)**.5
+    
