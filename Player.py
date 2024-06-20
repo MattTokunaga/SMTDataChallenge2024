bb1977eaac77a82e1e20287a1df98a34a672a68a
MattTokunaga <matttokunaga1@gmail.com>
2024-06-19 17:50:54 -0700

    added player class

---
+++ Player.py
@@ -0,0 +1,41 @@
+class Player:
+    
+    def __init__(self, player_id):
+        
+        self.player_id = player_id
+        self.levels_played = []
+        self.routes = []
+        self.positions = []
+
+    def get_id(self):
+        return self.player_id
+    
+    def get_levels(self): 
+        return self.levels_played
+    
+    def add_level(self, new_level):
+        try:
+            self.levels_played.append(new_level)
+            return True
+        except:
+            return False
+    
+    def get_routes(self):
+        return self.routes
+    
+    def add_route(self, new_route):
+        try:
+            self.routes.append(new_route)
+            return True
+        except:
+            return False
+        
+    def get_positions(self):
+        return self.positions
+    
+    def add_position(self, new_position):
+        try:
+            self.positions.append(new_position)
+            return True
+        except:
+            return False
