9ca422e41a1254a0de3f578da2b0cc3ae89176f6
MattTokunaga <matttokunaga1@gmail.com>
2024-06-19 18:10:49 -0700

    added route class

---
+++ Route.py
@@ -0,0 +1,11 @@
+class Route:
+
+    def __init__(self, play, player_pos, ball_pos, game_info):
+
+        self.player_id = play[play["event_code"] == 2]["player_position"].iloc[0]
+        self.df
+        self.player_position 
+
+    def create_df(self, play, player_pos, ball_pos, game_info):
+
+    
